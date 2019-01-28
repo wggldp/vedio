94a9400a9feb41ee13a703a91dddff082173632b
wggldp <15295738871@163.com>
2019-01-28 18:21:12 +0800

    11

---
+++ pages/collect/collect.vue
@@ -0,0 +1,40 @@
+<template>
+	<view class="content">
+		<view v-if="hasLogin" class="hello">
+		    <view class="title">
+		        您好 {{userName}}，您已成功登录。
+		    </view>
+		    <view class="ul">
+				
+		    </view>
+		</view>
+		<view v-if="!hasLogin" class="hello">
+		    <view class="title">
+		        您好 游客。
+		    </view>
+		    <view class="ul">
+		        <view>您当前未登录，无权访问我们的app!</view>
+		        <view>在 “我的” 中点击 “登录” 可以 “登录您的账户”</view>
+		    </view>
+		</view>
+	</view>
+</template>
+
+<script>
+	import {
+	    mapState
+	} from 'vuex'
+	
+	export default {
+		data() {
+			return {
+				
+			};
+		},
+		computed: mapState(['forcedLogin', 'hasLogin', 'userName']),
+	}
+</script>
+
+<style>
+
+</style>
